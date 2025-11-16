94e4d7820c4cb8c16eaefb6603200893c6c2a5e3
Kartik <Kartik110605@gmail.com>
2025-11-16 14:36:48 +0530

    Moved the mouse controll logic to the mousecontroller class

---
+++ server/control/mouse_control.py
@@ -0,0 +1,26 @@
+from pynput.mouse import Controller as PynputMouseController, Button
+
+class MouseController:
+    def __init__(self):
+        self.mouse = PynputMouseController()
+
+    def move(self, x, y):
+        self.mouse.position(x, y)
+
+    def click(self, x, y, button, action):
+        self.mouse.position(x, y)
+
+        match button:
+            case 1: button_obj = Button.left
+            case 2: button_obj = Button.right
+            case 3: button_obj = Button.middle
+            case _: raise IOError("Invalid button")
+
+        if(action == 1):
+            self.mouse.press(button_obj)
+        else: 
+            self.ouse.release(button_obj)
+
+    def scroll(self, x, y, delta):
+        self.mouse.position = (x, y)
+        self.mouse.scroll(0, delta)
